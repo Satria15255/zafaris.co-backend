8543cecd714a523e17ab917c4902aa37c2d8b5c4
satriazafarani <satriazafarani57@gmail.com>
2026-05-25 07:34:52 +0700

    update user controller : added getUserProfile

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,6 +40,21 @@ exports.loginUser = async (req, res) => {
   }
 };
 
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return;
+      res.status(404).json({ message: "User Not Found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 exports.updateProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { authMiddleware, adminMiddleware } = require("../middleware/authMiddleware");
-const { registerUser, loginUser, updateProfile, updatePassword } = require("../controllers/authController");
+const { registerUser, loginUser, getUserProfile, updateProfile, updatePassword } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/user/profile", authMiddleware, getUserProfile);
 router.put("/user/profile", authMiddleware, updateProfile);
 router.put("/user/change-password", authMiddleware, updatePassword);
 module.exports = router;

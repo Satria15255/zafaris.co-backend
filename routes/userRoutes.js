910e4bddee7de91d576f896cca885e6ce79b5939
satriazafarani <satriazafarani57@gmail.com>
2026-07-10 19:37:40 +0700

    update api call for use list

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const { authMiddleware, adminMiddleware } = require("../middleware/authMiddleware");
+const {
+	authMiddleware,
+	adminMiddleware,
+} = require("../middleware/authMiddleware");
 
-router.get("/user", authMiddleware, adminMiddleware, userController.getUser);
-router.get("/user/:id/transactions", authMiddleware, adminMiddleware, userController.getUserTransaction);
+router.get(
+	"/user-list",
+	authMiddleware,
+	adminMiddleware,
+	userController.getUser,
+);
+router.get(
+	"/user/:id/transactions",
+	authMiddleware,
+	adminMiddleware,
+	userController.getUserTransaction,
+);
 
 module.exports = router;

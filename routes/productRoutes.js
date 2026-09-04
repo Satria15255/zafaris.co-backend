221a2aa11b2133902e45183b91a83f2a3d883276
satriazafarani <satriazafarani57@gmail.com>
2026-09-04 07:23:10 +0700

    update product routes sturcture

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,24 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts, getLatestProducts, getBestSellerProducts, getProductById, createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
-const { authMiddleware, adminMiddleware } = require("../middleware/authMiddleware");
-const { getDailyDiscounts, createDailyDiscount } = require("../controllers/discountController");
+const {
+  getAllProducts,
+  getLatestProducts,
+  getBestSellerProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
+const {
+  authMiddleware,
+  adminMiddleware,
+} = require("../middleware/authMiddleware");
+const {
+  getDailyDiscounts,
+  createDailyDiscount,
+} = require("../controllers/discountController");
 const upload = require("../utils/storage");
 
 router.get("/", getAllProducts);
 router.get("/latest", getLatestProducts);
 router.get("/best-seller", getBestSellerProducts);
-router.get("/:id", getProductById);
 router.get("/discounts/today", getDailyDiscounts);
 router.get("/discounts/generate", createDailyDiscount);
-router.put("/:id", authMiddleware, adminMiddleware, upload.single("image"), updateProduct);
-router.post("/", authMiddleware, adminMiddleware, upload.single("image"), createProduct);
+router.get("/:id", getProductById);
+router.put(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  upload.single("image"),
+  updateProduct,
+);
+router.post(
+  "/",
+  authMiddleware,
+  adminMiddleware,
+  upload.single("image"),
+  createProduct,
+);
 router.delete("/:id", authMiddleware, adminMiddleware, deleteProduct);
 
 router.post("/generate-discount", async (req, res) => {
   await createDailyDiscount();
   res.send("Diskon di-generate");
 });
-
 
 module.exports = router;
